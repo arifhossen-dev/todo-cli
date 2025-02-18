57668915d3382be0db0d639c5a65f9ae12b9f539
arifhossen <ahak.bsl@gmail.com>
2025-02-18 18:34:23 +0600

    CLI command create

---
+++ src/cli.php
@@ -0,0 +1,30 @@
+#! /usr/bin/env php
+<?php
+
+require __DIR__ . '/../vendor/autoload.php';
+
+use TaskCli\TaskManager;
+
+$taskManger = new TaskManager();
+
+$command = $argv[1] ?? null;
+$arg1 = $argv[2] ?? null;
+$arg2 = $argv[3] ?? null;
+
+switch ($command) {
+    case 'add':
+        $taskManger->addTask($arg1);
+        break;
+    case 'list':
+        $taskManger->getAllTasks();
+        break;
+    case 'delete':
+        $taskManger->deleteTask($arg1);
+        break;
+    case 'update':
+        $taskManger->updateTask($arg1);
+        break;
+    default:
+        echo "Invalid command\n";
+        break;
+}
